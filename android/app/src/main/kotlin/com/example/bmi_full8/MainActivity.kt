86f04f43b3fcd75744d1da52851b4de177b38a06
abdraevaeleonora <abdraevaeleonora@gmail.com>
2022-09-25 17:58:54 +0600

    bmi_full8

---
+++ android/app/src/main/kotlin/com/example/bmi_full8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bmi_full8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/calculate_butoon.dart
@@ -0,0 +1,23 @@
+import 'package:bmi_full8/utils/app_colors.dart';
+import 'package:bmi_full8/utils/app_text.dart';
+import 'package:bmi_full8/utils/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class CalculateButton extends StatelessWidget {
+  const CalculateButton({Key? key, required this.onTap}) : super(key: key);
+  final void Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 70,
+        width: double.infinity,
+        color: AppColor.buttonColor,
+        child: const Center(
+          child: Text(AppText.calculate, style: AppTextStyle.titleStyle),
+        ),
+      ),
+    );
+  }
+}
